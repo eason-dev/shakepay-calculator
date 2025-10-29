762169b6502f4dcc75f96fd658b354696f791465
Eason Chang <eason@easonchang.com>
2025-10-29 02:24:44 -0600

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,10 +66,12 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.svg', type: 'image/svg+xml' },
-      { url: '/favicon.ico' }
+      { url: '/favicon.ico', sizes: '32x32' },
+      { url: '/favicon.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
-    shortcut: '/favicon.svg',
+    shortcut: '/favicon.ico',
     apple: '/apple-touch-icon.png',
   },
 };
